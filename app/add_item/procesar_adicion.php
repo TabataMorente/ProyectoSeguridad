0276de874ec0b6e71ae6a51b8c2fa9b0e8c03863
ikerfuot <ikerfuente005@gmail.com>
2025-10-21 16:32:07 +0200

    eliminar apuestas arreglado

--- app/add_item/procesar_adicion.php
+++ app/add_item/procesar_adicion.php
@@ -29,6 +29,7 @@ $cantidad  = isset($_POST['cantidad'])  ? $conn->real_escape_string($_POST['cant
 
 if (empty($cerdo) || empty($idCarrera) || empty($cantidad)) {
     echo "❌ Faltan datos del formulario.";
+    echo "<a href=" . "/items/items.php?user=" . $email . ">  Volver</a>";
     exit;
 }
 
@@ -42,6 +43,7 @@ $result_validacion = $conn->query($sql_validacion);
 
 if (!$result_validacion || $result_validacion->num_rows === 0) {
     echo "❌ Error: El cerdo no participa en la carrera seleccionada.";
+    echo "<a href=" . "/items/items.php?user=" . $email . ">  Volver</a>";
     exit;
 }
 
@@ -55,6 +57,7 @@ if ($conn->query($sql_apuesta) === TRUE) {
     echo "<a href=" . "/items/items.php?user=" . $email . ">  Volver</a>";
 } else {
     echo "❌ Error al registrar la apuesta: " . $conn->error;
+    echo "<a href=" . "/items/items.php?user=" . $email . ">  Volver</a>";
 }
 
 $conn->close();
