704f97b236aba2f17a3bc560a6f6a9308bb46906
Laura <lauracalvop@gmail.com>
2025-11-07 10:30:13 +0100

    evita que se guarde cache

--- app/pagUsuario/pagUsuario.php
+++ app/pagUsuario/pagUsuario.php
@@ -1,5 +1,10 @@
 <?php
 include('../auth.php');
+// Evitar caché del navegador
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: 0");
 
 	$nombre = "null";
         $email = "null";
